fff614e88c01ef4d9ed5f23615a430a0640e3e63
alanclarke <alan@qubit.com>
2021-05-28 17:10:20 +0100

    Audit + CI + coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,30 +1,63 @@
-var path = require('path')
+const path = require('path')
+
+const cfg = {
+  frameworks: ['mocha'],
+  files: ['test/*'],
+  preprocessors: {
+    'test/**/*.js': ['webpack', 'sourcemap']
+  },
+  webpackMiddleware: {
+    stats: 'errors-only',
+    logLevel: 'error'
+  },
+  reporters: ['progress'],
+  webpack: {
+    mode: 'development',
+    watch: true,
+    devtool: 'inline-source-map'
+  },
+  browsers: ['Chrome']
+}
 
 module.exports = function (config) {
-  config.set({
-    frameworks: [ 'mocha' ],
-    files: [ 'test/test-*' ],
-    preprocessors: {
-      '/**/*.js': [ 'webpack', 'sourcemap' ]
-    },
-    webpack: {
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            use: { loader: 'istanbul-instrumenter-loader' },
-            include: path.resolve('lib/')
-          }
-        ]
+  const coverage =
+    process.argv.indexOf('--coverage') > -1 || process.argv.indexOf('-c') > -1
+
+  if (coverage) {
+    enableCoverage(cfg)
+  }
+
+  config.set(cfg)
+}
+
+function enableCoverage (cfg) {
+  cfg.webpack.module = {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.join(__dirname, 'lib'), path.join(__dirname, 'dom')],
+        loader: 'istanbul-instrumenter-loader'
       }
-    },
-    webpackMiddleware: { stats: 'errors-only', logLevel: 'error' },
-    browsers: [ 'Chrome' ],
-    reporters: [ 'progress', 'coverage' ],
-    coverageReporter: {
-      type: 'html',
-      dir: 'coverage/',
-      reporters: [ { type: 'html' }, { type: 'text-summary' } ]
+    ]
+  }
+  cfg.reporters.push('coverage')
+  cfg.customLaunchers = {
+    ChromeCustom: {
+      base: 'ChromeHeadless',
+      flags: ['--headless', '--no-sandbox']
     }
-  })
+  }
+  cfg.coverageReporter = {
+    reporters: [{ type: 'html' }, { type: 'text' }],
+    check: {
+      global: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80
+      }
+    }
+  }
+  cfg.browsers = ['ChromeCustom']
+  return cfg
 }
